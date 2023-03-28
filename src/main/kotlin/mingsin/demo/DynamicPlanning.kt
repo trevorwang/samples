85f39e0d4122432ecef337fc114aed89b5233982
Trevor Wang <trevor.wang@qq.com>
2023-03-29 07:21:29 +0800

    feat: largest number at least twice other of others

--- src/main/kotlin/mingsin/demo/DynamicPlanning.kt
+++ src/main/kotlin/mingsin/demo/DynamicPlanning.kt
@@ -15,6 +15,26 @@ fun minCostClimbingStairs(costs: IntArray): Int {
     return dp[n]
 }
 
+
+fun dominantIndex(nums: IntArray): Int {
+    var m1 = 0
+    var m2 = 0
+    var index = 0
+
+    for (i in nums.indices) {
+
+        if (nums[i] > m1) {
+            m2 = m1
+            m1 = nums[i]
+            index = i
+        } else if (nums[i] > m2) {
+            m2 = nums[i]
+        }
+    }
+
+    return if (m1 > m2 * 2) index else -1
+}
+
 fun main() {
 
     println(minCostClimbingStairs(intArrayOf(1, 100, 1, 1, 1, 100, 1, 1, 100, 1)))
