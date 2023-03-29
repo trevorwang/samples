cb298d5e175ab55a50fbc155c09798962cb5717d
Trevor Wang <trevor.wang@qq.com>
2023-03-29 08:26:29 +0800

    feat: 杨辉三角

--- src/main/kotlin/mingsin/demo/DynamicPlanning.kt
+++ src/main/kotlin/mingsin/demo/DynamicPlanning.kt
@@ -35,7 +35,39 @@ fun dominantIndex(nums: IntArray): Int {
     return if (m1 > m2 * 2) index else -1
 }
 
+fun generatePascalsTriangle(numRows: Int): List<List<Int>> {
+    val result = mutableListOf<List<Int>>()
+//    result.add(listOf(1))
+//    if (numRows == 1) return result
+//    result.add(listOf(1, 1))
+//    if (numRows == 2) return result
+//    for (i in 2 until numRows) {
+//        result.add(mutableListOf<Int>().apply {
+//            add(1)
+//            val pre = result[i - 1]
+//            for (j in 0 until pre.size - 1) {
+//                add(pre[j] + pre[j + 1])
+//            }
+//            add(1)
+//        })
+//    }
+    for (i in 0 until numRows) {
+        val temp = mutableListOf<Int>()
+        for (j in 0..i) {
+            if (j == 0 || j == i) {
+                temp.add(1)
+            } else {
+                temp.add(result[i - 1][j] + result[i - 1][j - 1])
+            }
+        }
+        result.add(temp)
+    }
+    return result
+}
+
+
 fun main() {
 
-    println(minCostClimbingStairs(intArrayOf(1, 100, 1, 1, 1, 100, 1, 1, 100, 1)))
+//    println(minCostClimbingStairs(intArrayOf(1, 100, 1, 1, 1, 100, 1, 1, 100, 1)))
+    println(generatePascalsTriangle(5).map { it.toString() })
 }
