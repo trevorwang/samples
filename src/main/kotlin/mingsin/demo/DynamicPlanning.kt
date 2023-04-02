5d18de4386560eb7e6fb27d54e1c9955ba8b1a17
Trevor Wang <trevor.wang@qq.com>
2023-04-02 13:02:44 +0800

    删除并获取点数

--- src/main/kotlin/mingsin/demo/DynamicPlanning.kt
+++ src/main/kotlin/mingsin/demo/DynamicPlanning.kt
@@ -249,11 +249,59 @@ fun rob2(nums: IntArray): Int {
         }
         return dp[n]
     }
+
     val n = nums.size
     if (n == 1) return nums[0]
     val first = nums.take(n - 1).toIntArray()
     val last = nums.takeLast(n - 1).toIntArray()
     return max(robi(first), robi(last))
+}
+
+/**
+ * 删除并获得点数
+ * 给你一个整数数组 nums ，你可以对它进行一些操作。
+ *
+ * 每次操作中，选择任意一个 nums[i] ，删除它并获得 nums[i] 的点数。之后，你必须删除 所有 等于 nums[i] - 1 和 nums[i] + 1 的元素。
+ *
+ * 开始你拥有 0 个点数。返回你能通过这些操作获得的最大点数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [3,4,2]
+ * 输出：6
+ * 解释：
+ * 删除 4 获得 4 个点数，因此 3 也被删除。
+ * 之后，删除 2 获得 2 个点数。总共获得 6 个点数。
+ * 示例 2：
+ *
+ * 输入：nums = [2,2,3,3,3,4]
+ * 输出：9
+ * 解释：
+ * 删除 3 获得 3 个点数，接着要删除两个 2 和 4 。
+ * 之后，再次删除 3 获得 3 个点数，再次删除 3 获得 3 个点数。
+ * 总共获得 9 个点数。
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 2 * 104
+ * 1 <= nums[i] <= 104
+ */
+
+fun deleteAndEarn(nums: IntArray): Int {
+    val n = nums.max()
+    val sum = IntArray(n + 1) { 0 }
+    for (i in nums.indices) {
+        sum[nums[i]] += nums[i]
+    }
+    val dp = IntArray(n + 1) { 0 }
+    dp[1] = sum[1]
+    for (i in 2..n) {
+        dp[i] = max(dp[i - 1], dp[i - 2] + sum[i])
+    }
+    return dp[n]
 }
 
 fun main() {
@@ -272,4 +320,5 @@ fun main() {
 //    println(jump2(intArrayOf(2, 0, 0)))
 //    println(jump(intArrayOf(2, 0, 0)))
     println(rob2(intArrayOf(1, 2, 3, 1)))
+    println(deleteAndEarn(intArrayOf(1, 1, 1)))
 }
