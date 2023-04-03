295fdab33532509a6278f2338d32e87423972022
Trevor Wang <trevor.wang@qq.com>
2023-04-03 20:29:28 +0800

    最大子数组之和

--- src/main/kotlin/mingsin/demo/DynamicPlanning.kt
+++ src/main/kotlin/mingsin/demo/DynamicPlanning.kt
@@ -304,6 +304,48 @@ fun deleteAndEarn(nums: IntArray): Int {
     return dp[n]
 }
 
+/**
+ * 53. 最大子数组和
+ *
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 子数组 是数组中的一个连续部分。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ * 示例 2：
+ *
+ * 输入：nums = [1]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：nums = [5,4,-1,7,8]
+ * 输出：23
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 105
+ * -104 <= nums[i] <= 104
+ *
+ *
+ * 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。
+ */
+fun maxSubArray(nums: IntArray): Int {
+    val n = nums.size
+    val dp = IntArray(n) { Int.MIN_VALUE }
+    dp[0] = nums[0]
+    for (i in 1 until n) {
+        dp[i] = max(dp[i - 1] + nums[i], nums[i])
+    }
+    return dp.max()
+}
+
 fun main() {
 
 //    println(minCostClimbingStairs(intArrayOf(1, 100, 1, 1, 1, 100, 1, 1, 100, 1)))
@@ -319,6 +361,8 @@ fun main() {
 //    println(jump2(intArrayOf(2, 3, 1, 1, 4)))
 //    println(jump2(intArrayOf(2, 0, 0)))
 //    println(jump(intArrayOf(2, 0, 0)))
-    println(rob2(intArrayOf(1, 2, 3, 1)))
-    println(deleteAndEarn(intArrayOf(1, 1, 1)))
+//    println(rob2(intArrayOf(1, 2, 3, 1)))
+//    println(deleteAndEarn(intArrayOf(1, 1, 1)))
+
+    print(maxSubArray(intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4)))
 }
